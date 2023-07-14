fef0dca2e8ca1cab851536bc5894e10a914f1e5a
Ndugere <ndungudavid972@gmail.com>
2023-07-14 15:14:53 -0700

    checks if a character is a latter or not

---
+++ strings_mbili.c
@@ -0,0 +1,35 @@
+#include "simple_shell.h"
+
+/**
+ * _strcpy - Copies a string from source to destination
+ * @source: The source string
+ * @dest: The destination string
+ *
+ * Return: A pointer to the destination string
+ */
+char *_strcpy(char *dest, char *source)
+{
+	int i;
+
+	for (i = 0; source[i] != '\0'; i++)
+	{
+		dest[i] = source[i];
+	}
+	dest[i] = '\0';
+	return dest;
+}
+
+/**
+ * _isalpha - Checks if the input character is an alphabetic letter
+ * @c: The character to be checked
+ *
+ * Return: 1 if the character is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return SUCCESS;
+	}
+	return FAIL;
+}
